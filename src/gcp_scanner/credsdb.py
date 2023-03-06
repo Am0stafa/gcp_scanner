8dbe416d4402da6398319af7d9702580ff5cdbfa
Abdelrahman mostafa <a.abdo.mae@gmail.com>
2023-03-07 00:45:12 +0200

    added logs to credsdb

--- src/gcp_scanner/credsdb.py
+++ src/gcp_scanner/credsdb.py
@@ -190,12 +190,12 @@ def find_creds(explicit_path: Optional[str] = None) -> List[str]:
         search_paths.append(full_path)
 
   for dir_path in search_paths:
-    print(f"Scanning {dir_path} for credentials.db")
+    logging.info(f"Scanning {dir_path} for credentials.db")
     full_path = os.path.join(dir_path, "credentials.db")
     if os.path.exists(full_path) and os.access(full_path, os.R_OK):
-      print(f"Identified accessible gcloud config profile {full_path}")
+      logging.info(f"Identified accessible gcloud config profile {full_path}")
       list_of_creds_files.append(full_path)
-  print(f"Identified {len(list_of_creds_files)} credential DBs")
+  logging.info(f"Identified {len(list_of_creds_files)} credential DBs")
   return list_of_creds_files
 
 
